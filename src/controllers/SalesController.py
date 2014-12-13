4f72ebc511b8d3478f99826c3a827c8c33ea4732
ezequielo <ezequielra1@gmail.com>
2014-12-14 00:23:33 +0100

    refactor SalesController

--- src/controllers/SalesController.py
+++ src/controllers/SalesController.py
@@ -1,113 +1,161 @@
 from src.entities.base.SaleLine import SaleLine
+from src.utils.Menus import Menus
 
 __author__ = 'ezequiel'
 
 class SalesController:
+    """
+    SalesController class allows the application to create, list and remove sales
+    given an agent. It also allows to display agent's info
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def manage_sales(mc, org):
+        """
+        This is the main controller for managing sales. Its aim is
+        to redirect to specific functions
+        :param mc: MainController
+        :param org: Demo Organization
+        :return: Void
+        """
+        selected_agent = SalesController.select_agent(mc, org.agent_list)
+        while True:
+            option = Menus.sales_main_menu()
+            if option == 1:
+                SalesController.list_sales(selected_agent)
+            elif option == 2:
+                SalesController.create_sale(selected_agent, org)
+            elif option == 3:
+                SalesController.remove_sale(selected_agent)
+            elif option == 4:
+                SalesController.print_summary(selected_agent)
+            elif option == 0:
+                mc.menu_redirect()
+
+    @staticmethod
+    def select_agent(mc, agent_list):
+        """
+        Helper method for selecting agents given its ID
+        :param mc: MainController
+        :param agent_list: Agent list
+        :return: Chosen agent
+        """
+        selected_agent = None
+        a_id = raw_input("Agent ID: ")
+        for agent in agent_list:
+            if agent.agent_id == a_id:
+                selected_agent = agent
+        if selected_agent:
+            print("Agent " + selected_agent.agent_id + " selected")
+            return selected_agent
+        else:
+            print("Invalid ID !\n")
+            mc.menu_redirect()
 
 
     @staticmethod
-    def manageSales(mc, org):
-
-        ok = False
-
-        while ok == False:
-            selected_agent = None
-            print "-"*60
-            id = raw_input("Agent ID: ")
-
-            for agent in org.agent_list:
-                if agent.agent_id == id:
-                    selected_agent = agent
-
-            if selected_agent:
-                print "Agent " + selected_agent.agent_id + " selected"
-
-                ok2 = False
-                while ok2 == False:
-                    print "#"*60 + "\n" + "\t"*4 + "Sales Management\n" +"#"*60
-                    print "-"*60
-                    print "1. List sales"
-                    print "2. Create new sale"
-                    print "3. Remove sale"
-                    print "4. Summary"
-                    print "0. Back"
-                    print "-"*60
-                    option = int(raw_input("Option: "))
-                    print "-"*60
-
-                    if option == 1:
-                        print "Sales:\n"
-                        for sale in selected_agent.sales:
-                            print sale
-                            print "-"*30
-
-                    elif option == 2:
-
-                        lines = []
-                        exit = False
-                        while exit == False:
-                            print "-"*60
-                            print "Create new sale"
-                            print "-"*60
-                            print "1. Add line"
-                            print "2. Confirm sale"
-                            print "0. Back"
-                            sale_option = int(raw_input("Option: "))
-                            print "-"*60
+    def list_sales(agent):
+        """
+        Prints a list of sales of the current agent
+        :param agent: Current selected agent
+        :return: Void
+        """
+        print("Sales:\n")
+        for sale in agent.sales:
+            print(sale)
+            print("-"*30)
 
 
+    @staticmethod
+    def create_sale(agent, org):
+        """
+        Controller function for creating and adding sales to the current agent's sales list
+        :param agent: Selected agent
+        :param org: Organization
+        :return: Void
+        """
+        lines = []
+        menu_back = False
+        while menu_back == False:
+            option = Menus.create_sale_menu()
+            if option == 0:
+                menu_back = True
+            elif option == 1:
+                SalesController.add_line(org, lines)
+            elif option == 2:
+                SalesController.confirm_sale(lines, agent)
+                menu_back = True
 
-                            if sale_option == 0:
-                                exit = True
 
-                            elif sale_option == 1:
-                                selected_article = None
-                                while selected_article == None:
-                                    art_code = raw_input("Article code: ")
-                                    for category in org.categories:
-                                        for article in category.list_articulos:
-                                            if article.ean13 == art_code:
-                                                selected_article = article
-                                uds = int(raw_input("Units: "))
+    @staticmethod
+    def add_line(org, lines):
+        """
+        Helper function whose aim is to creating and adding SaleLines to the current sale
+        :param org: Organization
+        :param lines: List of lines in the current sale
+        :return: Void
+        """
+        selected_article = None
+        while selected_article == None:
+            art_code = raw_input("Article code: ")
+            for category in org.categories:
+                for article in category.list_articulos:
+                    if article.ean13 == art_code:
+                        selected_article = article
+        uds = int(raw_input("Units: "))
 
-                                line = SaleLine(uds, selected_article)
-                                lines.append(line)
+        line = SaleLine(uds, selected_article)
+        lines.append(line)
 
-                            elif sale_option == 2:
-                                if lines:
-                                    sale = selected_agent.crearVenta()
-                                    for line in lines:
-                                        sale.add_line(line)
-                                    print "Your sale has been successfully created:\n"
-                                    sale.printInvoice()
-                                    exit = True
 
-                    elif option == 3:
-                        print "\nSelect a sale to remove:\n"
+    @staticmethod
+    def confirm_sale(lines, agent):
+        """
+        Helper function that adds all lines and generates an invoice.
+        :param lines: List of SaleLines
+        :param agent: Current selected agent
+        :return: Void
+        """
+        if lines:
+            sale = agent.crearVenta()
+            for line in lines:
+                sale.add_line(line)
+            print("Your sale has been successfully created:\n")
+            sale.printInvoice()
 
-                        i = 1
-                        for sale in selected_agent.sales:
-                            print "\t#" +str(i) +" Date: "+ str(sale.get_date()) + " - State: "+sale.State + " - Total: " + str(sale.getTotal())
-                            i = i +1
+    @staticmethod
+    def remove_sale(agent):
+        """
+        This method allows users to delete a sale from the current agent
+        :param agent: Current agent
+        :return: Void
+        """
+        print("\nSelect a sale to remove:\n")
+        i = 1
+        for sale in agent.sales:
+            print("\t#" + str(i) + " Date: " + str(sale.get_date()) + " - State: "+sale.State + " - Total: " \
+                  + str(sale.get_total()))
+            i = i + 1
+        selection = 0
+        while selection < 1 or selection > i:
+            selection = int(raw_input("Sale number: "))
 
-                        selection = 0
-                        while selection <1 or selection > i:
-                            selection = int(raw_input("Sale number: "))
+        agent.sales.pop(selection-1)
 
-                        selected_agent.sales.pop(selection-1)
-
-                    elif option == 4:
-                        print "Agent Info\n"
-                        print "\t Name:\t" + selected_agent.firstname + " "+selected_agent.lastname
-                        print "\t Age:\t" + str(selected_agent.get_edad())
-                        print "\t Email:\t" + selected_agent.email
-                        print "\t Base:\t" + str(selected_agent.base)
-                        print "\t Commissions:\t" + str(selected_agent.getComisiones())
-                        print "\t Total:\t" + str(selected_agent.getTotal())
-                        print "-"*60
-
-                    elif option == 0:
-                        mc.printMenu()
-
-            else:
-                print "Invalid ID"
+    @staticmethod
+    def print_summary(agent):
+        """
+        Prints a summary of the current agent, including full name, email, age and salary
+        :param agent: Current selected agent
+        :return: Void
+        """
+        print("Agent Info\n")
+        print("\t Name:\t" + agent.firstname + " "+agent.lastname)
+        print("\t Age:\t" + str(agent.get_edad()))
+        print("\t Email:\t" + agent.email)
+        print("\t Base:\t" + str(agent.base))
+        print("\t Commissions:\t" + str(agent.getComisiones()))
+        print("\t Total:\t" + str(agent.getTotal()))
+        print("-"*60)
