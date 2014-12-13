9c266406747656f8adbeec9d28ed69ef08a43818
yoiiii121 <yoiiii121@gmail.com>
2014-12-13 20:00:27 +0100

    name changed, empty espace removed or added and empty line removed or added

--- src/entities/base/SaleLine.py
+++ src/entities/base/SaleLine.py
@@ -1,41 +1,34 @@
 __author__ = 'ezequiel'
 
+
 class SaleLine:
-
     # Constructor
-
     def __init__(self, uds, art):
         self.art = art
         self.uds = uds
 
     # Getter and setters
-
     def get_art(self):
         return self.art
 
     def set_art(self, article):
-        self.art=article
-
+        self.art = article
 
     def get_uds(self):
         return self.uds
 
     def set_uds(self, uds):
-        self.uds=uds
-
+        self.uds = uds
 
     # Main methods
-
-    def getSubtotal(self):
+    def get_subtotal(self):
         st = 0.0
         if self.get_art().get_promotion():
             promo = self.get_art().get_promotion()
-            st = self.get_uds()*self.get_art().get_listprice()*promo.get_perc()
+            st = self.get_uds() * self.get_art().get_list_price() * promo.get_perc()
         else:
-            st = self.get_uds()*self.get_art().get_listprice()
+            st = self.get_uds() * self.get_art().get_list_price()
         return st
 
-
-
     def __str__(self):
-        return str(self.art)+'uds: '+str(self.get_uds())+ ' subtotal: '+str(self.getSubtotal())
+        return str(self.art) + 'uds: ' + str(self.get_uds()) + ' subtotal: ' + str(self.get_subtotal())
