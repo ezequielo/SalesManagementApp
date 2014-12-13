bfe50ac5950a182bdd79d6e8e6ab0db5cd7ec67a
yoiiii121 <yoiiii121@gmail.com>
2014-12-13 20:43:41 +0100

    names changed, empty espaces removed or added and empty lines removed or added

--- src/entities/agents/Commission.py
+++ src/entities/agents/Commission.py
@@ -1,26 +1,24 @@
 __author__ = 'ezequiel'
 
+
 class Commission:
-
     # Constructor
-
-    def __init__(self, com_name, perc):
+    def __init__(self, com_name, percentage):
         self.com_name = com_name
-        self.perc = perc
+        self.percentage = percentage
 
     # Getters and setters
-
     def get_com_name(self):
         return self.com_name
 
     def set_com_name(self):
         return self.com_name
 
-    def get_perc(self):
-        return self.perc
+    def get_percentage(self):
+        return self.percentage
 
-    def set_perc(self, perc):
-        self.perc = perc
+    def set_percentage(self, percentage):
+        self.percentage = percentage
 
     def __str__(self):
-        return self.com_name+ " - " +str(self.perc)+"%"
+        return self.com_name + " - " + str(self.percentage) + "%"
