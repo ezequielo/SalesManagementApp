8d86b1f05053984d4ad77f53e8c720903ea44de6
yoiiii121 <yoiiii121@gmail.com>
2014-12-13 19:44:44 +0100

    name changed, empty espace removed or added and empty lines removed or added

--- src/entities/base/Category.py
+++ src/entities/base/Category.py
@@ -1,29 +1,28 @@
 __author__ = 'ezequiel'
 
+
 class Category:
-
     # Constructor
-
     def __init__(self, id_cat, cat_name, desc):
         self.cat_name = cat_name
-        self.id_cat=id_cat
+        self.id_cat = id_cat
         self.desc = desc
-        self.list_articulos = []
+        self.article_list = []
         self.commission = None
         self.org = None
 
     # Getter and setters
 
-    def get_list_articulos(self):
-        return self.list_articulos
+    def get_article_list(self):
+        return self.article_list
 
-    def set_list_articulos(self,list_articulos):
-        self.list_articulos=list_articulos
+    def set_article_list(self, article_list):
+        self.article_list = article_list
 
     def get_cat_name(self):
         return self.cat_name
 
-    def set_cat_name(self,cat_name):
+    def set_cat_name(self, cat_name):
         self.cat_name = cat_name
 
     def get_desc(self):
@@ -35,13 +34,13 @@ class Category:
     def get_id_cat(self):
         return self.id_cat
 
-    def set_id_cat(self,id_cat):
+    def set_id_cat(self, id_cat):
         self.id_cat = id_cat
 
     def get_commission(self):
         return self.commission
 
-    def set_commission(self,commission):
+    def set_commission(self, commission):
         self.commission = commission
 
     def get_org(self):
@@ -51,17 +50,16 @@ class Category:
         self.org = org
 
     # Main methods
+    def add_article(self, art):
+        self.article_list.append(art)
 
-    def add_articulo(self, art):
-        self.list_articulos.append(art)
-
-    def remove_articulo(self, art):
+    def remove_article(self, art):
         remove = False
-        for article in self.list_articulos:
+        for article in self.article_list:
             if article.ean13 == art.ean13:
                 remove = True
         if remove:
-            self.list_articulos.remove(art)
+            self.article_list.remove(art)
 
     def __str__(self):
         return str(self.id_cat) + " - "+self.cat_name + " desc\n " + self.desc
