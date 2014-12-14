f22b2fd6cceecbc8c35cf323b9b23a0c35887ced
yoiiii121 <yoiiii121@gmail.com>
2014-12-14 11:47:28 +0100

    Promotion documentation added

--- src/entities/promotions/Promotion.py
+++ src/entities/promotions/Promotion.py
@@ -4,6 +4,15 @@ __author__ = 'ezequiel'
 class Promotion:
     # Constructor
     def __init__(self, name, percentage, minim):
+        """
+
+        Init method for Promotion of an article
+
+        :param name: promotion name
+        :param percentage: percentage reduction of article
+        :param minim: minim reduction number
+
+        """
         self.name = name
         self.percentage = percentage
         self.minim = minim
@@ -13,40 +22,123 @@ class Promotion:
 
     # Getters and setters
     def get_name(self):
+        """
+        Get promotion name
+
+        :return: promotion name
+
+        """
         return self.name
 
     def set_name(self, name):
+        """
+
+        Update actual promotion name
+
+        :param name: New promotion name
+
+        """
         self.name = name
 
     def get_percentage(self):
+        """
+
+        Get reduction percentage of article
+
+        :return: reduction percentage
+
+        """
         return self.percentage
 
     def set_percentage(self, percentage):
+        """
+
+        Update actual reduction percentage of article
+
+        :param percentage: new reduction percentage
+
+        """
         self.percentage = percentage
 
     def get_minim(self):
+        """
+
+        Get minim reduction percentage of article
+
+        :return: minim reduction percentage
+
+        """
         return self.minim
 
     def set_minim(self, minim):
+        """
+
+        Update actual minim reduction percentage of article
+
+        :param minim: new minim reduction percentage
+
+        """
         self.minim = minim
 
     def get_org(self):
+        """
+
+        Get cif of organization
+
+        :return: cif
+
+        """
         return self.org
 
     def set_org(self, org):
+        """
+        Update actual cif of organization
+
+        :param org: new cif
+
+        """
         self.org = org
 
     def get_promotion(self):
+        """
+
+        Get promotion instance
+
+        :return: promotion
+
+        """
         return self.org
 
     def set_promotion(self, promotion):
+        """
+
+        Update actual promotion instance
+
+        :param promotion: new promotion
+
+        """
         self.promotion = promotion
 
     def __str__(self):
+        """
+
+        Print promotion information
+
+        :return: String instance
+
+        """
         return self.name + " - " + str(self.percentage)
 
     # Main methods
     def add_article(self, article):
+        """
+
+        Add new article to promotion
+
+        :param article:  new article
+        :return: if it correct what the article is added
+
+        """
         article.set_promotion(self)
         self.article_list.append(article)
         for art in self.article_list:
@@ -57,6 +149,14 @@ class Promotion:
                 return False
 
     def remove_article(self, article):
+        """
+
+        Remove an article to promotion
+
+        :param article:  exist article
+        :return: if it correct what the article is removed
+
+        """
         article.set_promotion(None)
         for art in self.article_list:
             if art.ean13 == article.ean13:
