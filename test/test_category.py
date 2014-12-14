a890430e0984ba1a02a5a1f990e992e268a7c7ec
ezequielo <ezequielra1@gmail.com>
2014-12-14 19:28:04 +0100

    docummentation and PEP 8

--- test/test_category.py
+++ test/test_category.py
@@ -1,17 +1,23 @@
 __author__ = 'Nacho-w7'
 
-from mockito import *
 from unittest import TestCase
 from src.entities.base.Article import Article
 from src.entities.base.Category import Category
-from src.controllers.DataLoader import DataLoader
-class Test_category(TestCase):
+
+
+class TestCategory(TestCase):
     """
     Test category
-    Test class for testing the most interesting method category
+    Test class for testing the most interesting methods from category
 
     """
+
     def test_remove_article(self):
+        """
+        Test remove article
+        Asserts that the article list is correctly updated after a remove operation
+        """
+
         art1 = Article("123776789087", "Article 1", 12.0, 20.0)
         cat1 = Category(001, "CAT1", "Sample Category")
         boolean = False
